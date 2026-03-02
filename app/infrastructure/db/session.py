0cb604e9e5a5cfe5e3aa0f9309bcf53ab524b7da
jing <jing2025jing@gmail.com>
2026-03-02 17:39:30 +0900

    feat: enhance database engine configuration with connection pool settings

--- app/infrastructure/db/session.py
+++ app/infrastructure/db/session.py
@@ -2,12 +2,20 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True,
+
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    pool_size=5,
+    max_overflow=10
+    )
 
 SessionLocal = async_sessionmaker(
     bind=engine,
     autocommit=False,
     autoflush=False,
-    # 비동기 환경에서 커밋 후 속성 접근 시 발생하는 MissingGreenlet 에러 방지 (객체 만료 방지)
     expire_on_commit=False,
 )
