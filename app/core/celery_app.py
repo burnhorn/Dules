3a46d81a2f9b5dc56478ca55175fc64e87edcbd1
jing <jing2025jing@gmail.com>
2026-03-23 23:31:54 +0900

    feat: add sentry sdk into celery worker app

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,6 +1,15 @@
 from celery import Celery
-
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
 from app.core.config import settings
+
+
+sentry_sdk.init(
+    dsn=settings.SENTRY_SDK_DSN,
+    integrations=[CeleryIntegration()],
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 celery_app = Celery(
     "dules_worker", broker=settings.REDIS_URL, backend=settings.REDIS_URL
@@ -12,7 +21,6 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="Asia/Seoul",
     enable_utc=True,
-
     worker_max_tasks_per_child=50,
     worker_max_memory_per_child=150000,     
     worker_prefetch_multiplier=1,
